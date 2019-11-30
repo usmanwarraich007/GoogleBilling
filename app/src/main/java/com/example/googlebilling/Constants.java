773942607a4a97c53d11563722bbe4ff443d83d8
Usman Warraich <usmanwarraich007@gmail.com>
2019-11-30 19:59:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/googlebilling/Constants.java
@@ -0,0 +1,17 @@
+package com.example.googlebilling;
+
+/**
+ * Created by delaroy on 11/20/17.
+ */
+
+public class Constants {
+
+    public static final String SKU_DELAROY_MONTHLY = "monthly_30";
+    public static final String SKU_DELAROY_THREEMONTH = "delaroy_threemonth";
+    public static final String SKU_DELAROY_SIXMONTH = "delaroy_sixmonth";
+    public static final String SKU_DELAROY_YEARLY = "delaroy_yearly";
+
+    public static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiYGHqxxYMw/E3NeI9nRmyzWWOlzKnNaR1r7nmjC+067lBZ9FQjbXnVBoXgY/LOfzAF+7l7xrMswD5SkGBslj5KqfvO043PtM08qGb17o3KYJ1SSG500peUfyD4/n8RWiY16SCZci3pveTXWW2bYmk0p/A45goqAvjfZYIN7QRgpqwO2zbn5IE/cqKd+/OqAP+LZnI+oXBMKw4Wbf9/6LkPQvAIaGnAljRE6VDlvNmo5GQaGn52q5/rVaPxNRR++zChloJ/lZ19Zpabk+rfDoHYPbj4x5LQIbSQolh9Kf/c7mMrdtLZ2Ta6g/U/37YuEQmJAw6g0isAJgQGAzgmowJQIDAQAB";
+
+
+}
